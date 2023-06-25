cd5a0fd3451b5a6cf15019d2c2319ea48338b200
Zadid Taqwa <Zadidtaqwa123@gmail.com>
2023-06-25 13:25:30 +0700

    initialize job-seek web client

---
+++ src/main/java/com/akiyaaa/JobSeekWebClientApp/JobSeekWebClientAppApplication.java
@@ -0,0 +1,13 @@
+package com.akiyaaa.JobSeekWebClientApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekWebClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekWebClientAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akiyaaa/JobSeekWebClientApp/JobSeekWebClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akiyaaa.JobSeekWebClientApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekWebClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
